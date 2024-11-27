18d93e4330b7e30535102224919d13ad9bc71368
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-11-27 21:43:48 +0545

    Add: button function

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = () => {
+  
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <>
-  
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={forGood} text="good"/>
+      <button onClick={forNeutral} text="neutral"/>
+      <button onClick={forBad} text="bad"/>
+
+    </div>
   )
 }
 
