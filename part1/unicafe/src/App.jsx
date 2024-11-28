2e85884e5af9ff37fa22ac70ca716506958d039f
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-11-28 16:02:25 +0545

    remove: display function

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 
-const Button = () => {
-  
-}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -11,12 +8,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const forGood = () =>{
+    setGood(good + 1)
+  };
+
+  const forNeutral = () =>{
+    setNeutral(neutral + 1)
+  };
+
+  const forBad = () =>{
+    setBad(bad + 1)
+  };
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={forGood} text="good"/>
-      <button onClick={forNeutral} text="neutral"/>
-      <button onClick={forBad} text="bad"/>
+      <button onClick={forGood}>good</button>
+      <button onClick={forNeutral}>neutral</button>
+      <button onClick={forBad}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad}</p>
 
     </div>
   )
