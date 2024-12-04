20694f5b32023ff68f9b88dcbe5b7b5718bbfe99
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-04 22:31:41 +0545

    feat: shows statistical info of gathered feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,18 +6,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const forGood = () =>{
     setGood(good + 1)
+    const updatedGood = good + 1;
+    setAll(updatedGood + neutral + bad)
   };
 
   const forNeutral = () =>{
     setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1;
+    setAll(good + updatedNeutral + bad)
+
   };
 
   const forBad = () =>{
     setBad(bad + 1)
+    const updatedBad = bad + 1;
+    setAll(good + neutral + updatedBad)
   };
+
+  const average = (good - bad) / all;
+  const positivePercentage = (good / all) * 100;
+
 
   return (
     <div>
@@ -30,6 +42,9 @@ const App = () => {
       <p>good {good} </p>
       <p>neutral {neutral} </p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercentage}</p>
 
     </div>
   )
