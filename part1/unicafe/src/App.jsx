d78e060739b6d97f9d5d2414a1b3945704c4daa8
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-05 12:50:18 +0545

    Refactor:  displaying statistics is extracted into its own Statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,29 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad, all}) => {
+
+  all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positivePercentage = (good / all) * 100;
+
+  return(
+    <>
+      <h1>statistics</h1>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercentage}</p>  
+    </>
+  )
+}
+
+const Button = ({onClick, text}) =>{
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -8,44 +32,24 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
 
-  const forGood = () =>{
-    setGood(good + 1)
-    const updatedGood = good + 1;
-    setAll(updatedGood + neutral + bad)
-  };
-
-  const forNeutral = () =>{
-    setNeutral(neutral + 1)
-    const updatedNeutral = neutral + 1;
-    setAll(good + updatedNeutral + bad)
-
-  };
-
-  const forBad = () =>{
-    setBad(bad + 1)
-    const updatedBad = bad + 1;
-    setAll(good + neutral + updatedBad)
-  };
-
-  const average = (good - bad) / all;
-  const positivePercentage = (good / all) * 100;
-
+  const handleGoodBtn = () => setGood(good + 1)
+  const handleNeutralBtn = () => setNeutral(neutral + 1)
+  const handleBadBtn = () => setBad(bad + 1)
 
   return (
     <div>
 
       <h1>give feedback</h1>
-      <button onClick={forGood}>good</button>
-      <button onClick={forNeutral}>neutral</button>
-      <button onClick={forBad}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercentage}</p>
-
+      <Button onClick={handleGoodBtn} text="Good" />
+      <Button onClick={handleNeutralBtn} text="Neutral" />
+      <Button onClick={handleBadBtn} text="Bad" />
+      <Statistics 
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          all={all}
+      />
+      
     </div>
   )
 }
