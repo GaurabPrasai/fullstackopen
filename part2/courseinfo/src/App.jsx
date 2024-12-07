d14c941f58d8af90107ec6ba3f02cc8ad4609d72
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-07 22:00:29 +0545

    Reafactor: refactor the code into the Course component

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React from "react"
 
 const Header = ({ course }) => <h1>{course}</h1>
 
@@ -11,16 +11,20 @@ const Part = ({ part }) =>
 
 const Content = ({ parts }) => 
   <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
   </>
+
+const Course = ({course}) => {
+  return(
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      
+    </>
+  )
+}
 
 const App = () => {
   const course = {
