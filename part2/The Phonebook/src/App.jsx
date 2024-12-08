4354a0eea0b6682b3fbfe33612ba752b26a1d86c
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-08 23:06:47 +0545

    feat: Prevent the user from being able to add names that already exist in the phonebook

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -1,41 +1,52 @@
-import { useState } from 'react'
-import Name from "./components/Name"
+import { useState } from "react";
+import Name from "./components/Name";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
-  const handleNameChange = (e) =>{
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
-  }
+  };
 
   const addNames = (e) => {
     e.preventDefault();
-    const names = {
-      name : newName
+
+    const isDuplicate = persons.some(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
+
+    if (isDuplicate) {
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
-    setPersons(persons.concat(names))
-    setNewName('');
-  }
+
+    const names = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(names));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNames}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      
-      {persons.map((person) => <Name key={person.name} name={person.name}/>)}
-      
-    </div>
-  )
-}
 
-export default App
+      {persons.map((person) => (
+        <Name key={person.name} name={person.name} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
