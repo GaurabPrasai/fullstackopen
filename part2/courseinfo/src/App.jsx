b1f6cfef166d96dced3fa527dca74e8ef853a9fd
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-08 21:12:20 +0545

    chore: formatted the code

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,53 +1,52 @@
-import React from "react"
-import Course from './Course'
+import React from "react";
+import Course from "./Course";
 
 const App = () => {
   const courses = [
     {
-      name: 'Half Stack application development',
+      name: "Half Stack application development",
       id: 1,
       parts: [
         {
-          name: 'Fundamentals of React',
+          name: "Fundamentals of React",
           exercises: 10,
-          id: 1
+          id: 1,
         },
         {
-          name: 'Using props to pass data',
+          name: "Using props to pass data",
           exercises: 7,
-          id: 2
+          id: 2,
         },
         {
-          name: 'State of a component',
+          name: "State of a component",
           exercises: 14,
-          id: 3
+          id: 3,
         },
         {
-          name: 'Redux',
+          name: "Redux",
           exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
+          id: 4,
+        },
+      ],
+    },
     {
-      name: 'Node.js',
+      name: "Node.js",
       id: 2,
       parts: [
         {
-          name: 'Routing',
+          name: "Routing",
           exercises: 3,
-          id: 1
+          id: 1,
         },
         {
-          name: 'Middlewares',
+          name: "Middlewares",
           exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]        
-  return <Course course={courses} />
+          id: 2,
+        },
+      ],
+    },
+  ];
+  return <Course course={courses} />;
+};
 
-}
-
-export default App
+export default App;
--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,34 +1,39 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => <h1>{course}</h1>;
 
-const Total = ({ parts }) => <p><b>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
+const Total = ({ parts }) => (
+  <p>
+    <b>
+      total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
+    </b>
+  </p>
+);
 
-const Part = ({ part }) => 
+const Part = ({ part }) => (
   <p>
     {part.name} {part.exercises}
   </p>
+);
 
-const Content = ({ parts }) => 
+const Content = ({ parts }) => (
   <>
     {parts.map((part) => (
       <Part key={part.id} part={part} />
-
     ))}
-    
   </>
+);
 
-const Course = ({course}) => {
-  return(
+const Course = ({ course }) => {
+  return (
     <>
-      {course.map(singleCourse => (
+      {course.map((singleCourse) => (
         <div key={singleCourse.id}>
           <Header course={singleCourse.name} />
           <Content parts={singleCourse.parts} />
           <Total parts={singleCourse.parts} />
         </div>
       ))}
-       
     </>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
