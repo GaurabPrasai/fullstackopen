f0720a3bdfb50ae27d06e4f9ef80e6f07be90e41
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-09 11:48:39 +0545

    feat: implementing the filter feature

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "021-560673" }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
   const [newPerson, setNewPerson] = useState({name: "", number: ""});
 
   const handlePersonChange = (e) => {
@@ -31,6 +36,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input name="nameFilter"/>
+      </div>
       <form onSubmit={addPersons}>
         <div>
           name: <input name="name" value={newPerson.name} onChange={handlePersonChange} />
