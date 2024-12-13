217688fa9c07dc54a675f6f6718a4a985dbc27e5
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-13 15:58:09 +0000

    Dec 13, 2024, 9:43 PM

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from axios
 import Name from "./components/Name";
 import AddNewPerson from "./components/AddNewPerson";
 import FilterPerson from "./components/FilterPersons";
